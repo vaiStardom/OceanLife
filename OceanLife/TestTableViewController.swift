34c5fe1c00fa0ea0bdb2573a6e86bfb197864177
Paul Addy <paul.addy@me.com>
2016-10-17 18:38:25 +0800

    adding a test view for ui tests

---
+++ OceanLife/TestTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TestTableViewController.swift
+//  OceanLife
+//
+//  Created by Paul Addy on 2016-10-17.
+//  Copyright © 2016 Paul Addy. All rights reserved.
+//
+
+import expanding_collection
+import UIKit
+
+class TestTableViewController: ExpandingTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
